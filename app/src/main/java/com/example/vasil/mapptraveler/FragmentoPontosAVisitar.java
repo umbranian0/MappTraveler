e0bd436bea3fe5af19e68fd932eff3d1780f5c3f
Vasile timotin <umbranian0@hotmail.com>
2017-12-25 13:36:38 +0000

    25/12/2017 - Fragmentos criados com o seu layout , ainda nao contem funcionalidades

--- app/src/main/java/com/example/vasil/mapptraveler/FragmentoPontosAVisitar.java
+++ app/src/main/java/com/example/vasil/mapptraveler/FragmentoPontosAVisitar.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentPontosAVisitar extends Fragment {
+public class FragmentoPontosAVisitar extends Fragment {
 
 
-    public FragmentPontosAVisitar() {
+    public FragmentoPontosAVisitar() {
         // Required empty public constructor
     }
 
@@ -23,10 +23,7 @@ public class FragmentPontosAVisitar extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_pontos_a_visitar, container, false);
-
-
-        return v;
+        return inflater.inflate(R.layout.fragment_fragmento_pontos_avisitar, container, false);
     }
 
 }
