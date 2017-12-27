2c4e3bc4a8c7c3aa8e64788b052bc0610ec1096c
Vasile timotin <umbranian0@hotmail.com>
2017-12-27 21:16:17 +0000

    FRAGMENT MAp ja contem mapa

--- app/src/main/java/com/example/vasil/mapptraveler/FragmentMap.java
+++ app/src/main/java/com/example/vasil/mapptraveler/FragmentMap.java
@@ -2,17 +2,31 @@ package com.example.vasil.mapptraveler;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentMap extends Fragment {
+public class FragmentMap extends Fragment implements OnMapReadyCallback {
 
+    //api key da google
+    GoogleMap map;
 
     public FragmentMap() {
         // Required empty public constructor
@@ -22,22 +36,38 @@ public class FragmentMap extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //criar uma view
         View view= inflater.inflate(R.layout.fragment_fragment_map, container, false);
-
-        // NOTE : We are calling the onFragmentInteraction() declared in the MainActivity
-        // ie we are sending "Fragment 1" as title parameter when fragment1 is activated
-
-     /*   if (mListener != null) {
-            mListener.onFragmentInteraction("Fragment Pontos a visistar");
-        }
-        */
-
-        // Here we will can create click listners etc for all the gui elements on the fragment.
-        // For eg: Button btn1= (Button) view.findViewById(R.id.frag1_btn1);
-        // btn1.setOnclickListener(...
 
         return view;
     }
+    //traz o mapa para o fragmento
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //trazer o mapa do fragmento
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().
+                findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        MapsInitializer.initialize((getContext()));
+
+        // Other supported types include: MAP_TYPE_NORMAL,
+        // MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        map.addMarker(new MarkerOptions().position(new LatLng(40.68, -74.04)).title("State of Lyberty").snippet("hopw you go there!"));
+
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(40.68, -74.04)).zoom(16).bearing(0).tilt(45).build();
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+    }
+
 
     public interface OnFragmentInteractionListener {
     }
